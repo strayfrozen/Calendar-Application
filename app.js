61b5b6c64a107c1ba655868c1cf87f14819a28d4
Stray <strayfrozen@yahoo.com>
2021-11-11 17:49:28 -0600

    Added moment to project and added current date to top of the project

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+//1 Get the current day at the top of the scheduler
+
+//baby steps study moment.js and figure out how to get the current day
+console.log(moment());
+//install moment.js and test it
+
+//Dynamically added the date with moment
+var currentTime = document.getElementById("currentDay");
+currentTime.innerText = moment().format('dddd , MMMM Do YYYY');
+
+//2//Create time blocks that have the current buisness hour
+//baby steps create Time blocks
+// add a 9 to 5 buisness hour on them with moment 
+
+
+
+//Things to do
+//1 Get the current day at the top of the scheduler
+//baby steps study moment.js and figure out how to get the current day
+//install moment.js and test it
+//completed
+
+//2//Create time blocks that have the current buisness hour
+//baby steps create Time blocks
+// add a 9 to 5 buisness hour on them
+
+//3time blocks are color coded based on the time if its passed or not
+//baby steps moment.js to find the time
+// based on the time change color of the time block
+
+//4 when I click on timeblock then I enter an event
+// make a click event with a submit event
+
+
+//5 when i submit event then the text is saved to local storage
+//baby steps use local storage method above varied to what I want to do
+
+//6 when refreshed the time stays in that block
+
+//study code for local storage
+
 const inputEl = document.getElementById('myTask')
 const form = document.querySelector('form')
 let taskUlEl = document.getElementById('taskList')
@@ -5,12 +46,9 @@ let clearBtnEl = document.getElementById('clearTasks')
 let myTasks = []
 let tasks = localStorage.getItem('myTasks')
 						? JSON.parse(localStorage.getItem('myTasks'))
-						: localStorage.setItem('myTasks', JSON.stringify(myTasks))
-// if (tasks) {
-// 	tasks = JSON.parse(localStorage.getItem('myTasks'))
-// } else {
-// 	localStorage.setItem('myTasks', JSON.stringify(myTasks))
-// }
+                        : localStorage.setItem('myTasks', JSON.stringify(myTasks))
+                        
+//Creates Tasks and save them to local storage
 function populateTasks() {
 	if (tasks.length > 0) {
 		tasks.forEach(task => {
@@ -32,7 +70,8 @@ function handleSubmitEvent(ev) {
 	tasks.push(inputValue)
 	localStorage.setItem('myTasks', JSON.stringify(tasks))
 	let liEl = document.createElement('li')
-	liEl.innerText = inputValue
+    liEl.innerText = inputValue
+    liEl.className = "liE1Style"
 	taskUlEl.appendChild(liEl)	
 	inputEl.value = ''
 }
